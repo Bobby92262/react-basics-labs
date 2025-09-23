80d6a1bd3998d2e580facabf99f90e34a7b33a22
bobby <bobbyfinn086@gmail.com>
2025-09-23 12:11:25 +0100

    Mark test as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -6,19 +6,30 @@ import React, {useState} from 'react';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      {title: "Dishes", description: "Empty dishwasher", deadline: "Today"},
-      {title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow"},
-      {title: "Tidy up", deadline:"Today"}
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false},
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false},
+      { id: 3, title: "Tidy up", deadline:"Today", done: false}
     ]
   });
+
+  const doneHandler = (taskIndex) => { 
+    const tasks = [...taskState.tasks]; //spread operator used to copy
+    tasks[taskIndex].done = !tasks[taskIndex].done; //sets it to opposit of its state
+    setTaskState({tasks}); //set state to updated tasks array
+    //console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => ( //map() function on tasks array in the taskState object
+      {taskState.tasks.map((task, index) => ( //map() function on tasks array in the taskState object
         <Task //Arrow function into the map method, param 'task' represents "task" array element
           title={task.title} //For every task in taskState.tasks array create a <Task /> component
-          description={task.description} //PAss proprs for the current task (title,description,deadline)
+          description={task.description} //Pass props for the current task (title,description,deadline)
           deadline={task.deadline}
+          key={task.id}
+          done={task.done}
+          markDone={() => doneHandler(index)} //index denotes the position of the task in the taskState.tasks array -> Indentify which task clicked
         />
       ))}
     </div>
